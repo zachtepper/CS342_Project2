8015f7ccac5b37eccd08b72f7447d68d1d30e61d
Zach Tepper <zach.tepp@gmail.com>
2021-02-27 18:27:02 -0600

    Added src files.

---
+++ sp2021Project2_connect4/src/main/java/AllScenes.java
@@ -0,0 +1,20 @@
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+
+// class containing all scenes needed for the program
+public class AllScenes {
+
+    public Scene welcomeScene() {
+        Scene scene = new Scene(new VBox(), 700,700);
+    }
+
+    public Scene gamePlayScene() {
+
+    }
+
+    public Scene winMessageScene() {
+
+    }
+
+
+}
