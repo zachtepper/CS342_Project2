d44e7579c38b643125395e9c58bac6f2ba03d6d6
Zach Tepper <zach.tepp@gmail.com>
2021-03-01 12:34:53 -0600

    Added welcome scene

--- sp2021Project2_connect4/src/main/java/AllScenes.java
+++ sp2021Project2_connect4/src/main/java/AllScenes.java
@@ -1,20 +1,46 @@
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 // A class containing all scenes needed for the program
+
 public class AllScenes {
 
-    public Scene welcomeScene() {
-        Scene scene = new Scene(new VBox(), 700,700);
+    public static Scene welcomeScene() {
+        // Text object to create a title
+        Text welcome_text = new Text("Welcome to Connect Four");
+
+        // will start the game when clicked
+        Button start_game_button = new Button("Start Game");
+        // handler to switch scenes and start game
+        start_game_button.setOnAction((e) -> {
+            // put code here to switch scenes
+            System.out.println("Starting game..");
+        });
+
+        // placing all elements into a BorderPane object
+        BorderPane start_menu = new BorderPane(start_game_button, welcome_text, null, null, null);
+
+        Scene scene = new Scene(new VBox(start_menu), 700,700);
+        return scene;
     }
 
-    public Scene gamePlayScene() {
+    public static Scene gamePlayScene() {
+        Scene scene;
+
+        Text gameplayText = new Text("Gameplay Screen.");
+
+        BorderPane gameplay_scene = new BorderPane(null, gameplayText, null, null, null);
+
+        scene = new Scene(new VBox(gameplay_scene), 700,700);
+        return scene;
+    }
+    /*
+    public static Scene winMessageScene() {
 
     }
-
-    public Scene winMessageScene() {
-
-    }
-
+    */
 
 }
--- sp2021Project2_connect4/src/main/java/JavaFXTemplate.java
+++ sp2021Project2_connect4/src/main/java/JavaFXTemplate.java
@@ -2,28 +2,38 @@ import javafx.application.Application;
 
 import javafx.scene.Scene;
 
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.util.HashMap;
+
 public class JavaFXTemplate extends Application {
+	public static HashMap<String, Scene> sceneMap = new HashMap<String, Scene>();
+	public static Scene currentScene = new Scene(new VBox(), 700, 700);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
 	}
 
-	//feel free to remove the starter code from this method
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		primaryStage.setTitle("Welcome to JavaFX");
-		
-		
-		
-				
-		Scene scene = new Scene(new VBox(), 700,700);
-		primaryStage.setScene(scene);
+		primaryStage.setTitle("Connect Four");
+
+		// place all scenes into the hash map
+		sceneMap.put("welcome", AllScenes.welcomeScene());
+		sceneMap.put("gameplay", AllScenes.gamePlayScene());
+
+		// start program with welcome scene
+		currentScene = sceneMap.get("welcome");
+
+		primaryStage.setScene(currentScene);
 		primaryStage.show();
 	}
+
 
 }
