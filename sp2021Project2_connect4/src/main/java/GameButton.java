6510ebef75f3fe40fe6636c36c072c81b3fac75a
lillyyc <60705507+lillyyc@users.noreply.github.com>
2021-03-02 18:44:39 -0600

    Create GameButton.java

---
+++ sp2021Project2_connect4/src/main/java/GameButton.java
@@ -0,0 +1,37 @@
+import javafx.scene.control.Button;
+
+// feel free to edit this etc. this was just outline kinda?
+public class GameButton extends Button {
+	
+	// pos variable??
+	private int row;
+	private int col;
+	private boolean clicked;
+	
+	GameButton(int row, int col) {
+		this.row = row;
+		this.col = col;
+		clicked = false;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getCol() {
+		return col;
+	}
+	
+	public boolean isClicked() {
+		return clicked;
+	}
+	
+	public void setPos(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+	
+	public void click() {
+		clicked = true;
+	}
+}
